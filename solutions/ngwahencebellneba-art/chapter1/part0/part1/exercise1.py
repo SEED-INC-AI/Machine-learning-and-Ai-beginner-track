f8d97951a7cabe112c34b975707ca9b327f75a75
ngwahencebellneba-art <ngwahencebellneba@gmail.com>
2026-02-28 08:55:27 +0100

    (ngwahencebellneba-art):finished exercise1 part0 of chapter1

---
+++ solutions/ngwahencebellneba-art/chapter1/part0/part1/exercise1.py
@@ -0,0 +1,10 @@
+name = "Ngwa Hencebell" #string
+age = 21     #interger
+height = 1.60    #float(in meters)
+is_student = True   #boolean
+print("person's profile:")
+print("----------------------")
+print(f"Name:{name} - Type: {type(name)}")
+print(f"Age: {age} - Type:{type(age)}")
+print(f"Height: {height} meters - Type: {type(height)}")
+print(f"Is Student: {is_student} - Type: {type(is_student)}")
